ab1f4ca99f4ea5c48827e9debab17b54844f6dfa
Komal <waghmarekomal21@gmail.com>
2022-08-31 19:39:46 +0530

    [Komal] Prime Number

---
+++ PrimeNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        int count = 0;
+        public void PrimeNumCheck()
+        {
+            Console.WriteLine("Enter the number to check either it is prime number or not");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                    count++;
+            }
+            if (count == 2)
+                Console.WriteLine("It is a prime number");
+            else
+                Console.WriteLine("It is not a prime number");
+        }
+    }
+}
