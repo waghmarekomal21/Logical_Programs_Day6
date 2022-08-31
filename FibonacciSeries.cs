5dbefdb84db6bc98eeb6f6a3f462864cf8f34266
Komal <waghmarekomal21@gmail.com>
2022-08-31 19:33:29 +0530

    [Komal] Fibonacci Program

---
+++ FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        int firstNum = 0, secondNum = 1, thirdNum = 0;
+        public void Series()
+        {
+            Console.WriteLine("Enter the number to print fibonacci series");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(firstNum + "\n" + secondNum);
+            while (num > 0)
+            {
+                thirdNum = firstNum + secondNum;
+                Console.WriteLine(thirdNum);
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                num--;
+            }
+        }
+    }
+}
