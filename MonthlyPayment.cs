e347f8f3f018bb6ffc51119d9db81fd84132e953
Komal <waghmarekomal21@gmail.com>
2022-09-03 23:46:23 +0530

    [Komal] Monthly Payment

---
+++ MonthlyPayment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class MonthlyPayment
+    {
+            double P, Y, R, n, r, payment;
+            public void Payment()
+            {
+                Console.WriteLine("Enter principle amount", P);
+                P = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter number of year", Y);
+                Y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter rate of interest", R);
+                R = Convert.ToDouble(Console.ReadLine());
+                n = (12 * Y);
+                Console.WriteLine("Number: " + n);
+                r = R / (12 * 100);
+                Console.WriteLine("rate: " + r);
+                payment = ((P * r) / (1 - Math.Pow(1 + r, -n)));
+                Console.WriteLine("Monthly Payment: " + payment);
+            }
+        }
+    }
+
